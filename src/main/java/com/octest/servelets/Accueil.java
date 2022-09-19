f0bd2f940510f0e4b439a73562bc69b1e3d15309
gloire <gloireondongo1205@gmail.com>
2022-09-19 21:00:34 +0000

    test

--- src/main/java/com/octest/servelets/Accueil.java
+++ src/main/java/com/octest/servelets/Accueil.java
@@ -27,7 +27,7 @@ public class Accueil extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/views/accueil.jsp").forward(request, response);
 
 		
 	}
