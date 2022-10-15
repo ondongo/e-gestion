bab56a3fa4b747798a4e051745697c47b2255c97
moustaphafal <tontonfall5@gmail.com>
2022-10-15 18:56:37 +0000

    fixing servlet issues

--- src/main/java/com/octest/servelets/Reglage.java
+++ src/main/java/com/octest/servelets/Reglage.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Reglage
  */
-@WebServlet("/Reglage")
+//@WebServlet("/Reglage")
 public class Reglage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
