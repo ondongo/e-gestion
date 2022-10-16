b4108bb5de159d33ab82014cf90b12813a944299
moustaphafal <tontonfall5@gmail.com>
2022-10-16 13:08:05 +0000

    fixing issues

--- src/main/java/com/octest/servelets/Reglage.java
+++ src/main/java/com/octest/servelets/Reglage.java
@@ -10,31 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Reglage
  */
-//@WebServlet("/Reglage")
+@WebServlet("/Reglage")
 public class Reglage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public Reglage() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	    this.getServletContext().getRequestDispatcher("/reglage.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
