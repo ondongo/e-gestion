0857e0e7f66443e2e950fe3189011e22dd512199
moustaphafal <tontonfall5@gmail.com>
2022-10-16 13:21:23 +0000

    fixing issues

--- src/main/java/com/octest/servelets/HttpServletRequest.java
+++
@@ -1,5 +0,0 @@
-package com.octest.servelets;
-
-public class HttpServletRequest {
-
-}
--- src/main/java/com/octest/servelets/Index.java
+++ src/main/java/com/octest/servelets/Index.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/Index")
+//@WebServlet("/Index")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
